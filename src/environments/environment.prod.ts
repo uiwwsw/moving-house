ad57c77ee3c7d7e6803edb75e8b6d02c3d067f0b
uiwwsw <uiwwsw@icloud.com>
2022-03-15 23:06:56 +0900

    init
    Signed-off-by: uiwwsw <uiwwsw@icloud.com>

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'moving-house-7ec8d',
+    appId: '1:582282545728:web:550f8662cdbca8626bada5',
+    storageBucket: 'moving-house-7ec8d.appspot.com',
+    apiKey: 'AIzaSyDEcA6uZht0WcLK7bTNPBgMkQQsoyB-IN0',
+    authDomain: 'moving-house-7ec8d.firebaseapp.com',
+    messagingSenderId: '582282545728',
+    measurementId: 'G-LBN1L1292Y',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'moving-house-7ec8d',
+    appId: '1:582282545728:web:550f8662cdbca8626bada5',
+    storageBucket: 'moving-house-7ec8d.appspot.com',
+    apiKey: 'AIzaSyDEcA6uZht0WcLK7bTNPBgMkQQsoyB-IN0',
+    authDomain: 'moving-house-7ec8d.firebaseapp.com',
+    messagingSenderId: '582282545728',
+    measurementId: 'G-LBN1L1292Y',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
